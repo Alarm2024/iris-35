568a90a22e2dd64e5318d0d62d37acb3d649fd99
35 <172379467+Alarm2024@users.noreply.github.com>
2026-09-13 01:17:00 +0300

    Fix address order LTR and add Zamalek line

---
+++ addr.js
@@ -0,0 +1,7 @@
+(function(){
+  var f=document.querySelector("footer");
+  if(!f)return;
+  f.innerHTML='<a href="mailto:support@elghaly.dev">support@elghaly.dev</a>'
+    +'<p class="addr" dir="ltr">548 Market Street, San Francisco, California 94104</p>'
+    +'<p class="addr" dir="ltr">35 Zamalek, Cairo, Egypt</p>';
+})();
