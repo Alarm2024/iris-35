26a18c6879d5f3ceb193fa83ade71e17a0708616
35 <172379467+Alarm2024@users.noreply.github.com>
2026-09-15 03:49:42 +0300

    Add deskSigner and Squads to IRIS drop footer

--- addr.js
+++ addr.js
@@ -2,6 +2,8 @@
   var f=document.querySelector("footer");
   if(!f)return;
   f.innerHTML='<a href="mailto:support@elghaly.dev">support@elghaly.dev</a>'
+    +'<p class="addr" dir="ltr">deskSigner <a href="https://solscan.io/account/3BZGNtr7AQ5c6Rf7nUhunfvqooQAtb5Eaek9Hw1npbLo">3BZGNtr7AQ5c6Rf7nUhunfvqooQAtb5Eaek9Hw1npbLo</a></p>'
+    +'<p class="addr" dir="ltr">Squads <a href="https://solscan.io/account/GMyuRJbwPTF5pEHvMCNJqujoLk8tZCdFY6i9feMoczcQ">GMyuRJbwPTF5pEHvMCNJqujoLk8tZCdFY6i9feMoczcQ</a></p>'
     +'<p class="addr" dir="ltr">548 Market Street, San Francisco, California 94104</p>'
     +'<p class="addr" dir="ltr">35 Zamalek, Cairo, Egypt</p>';
 })();
